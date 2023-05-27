545d2247c6026c9fe6ad5112899f202f89693bd2
kepha-okari <kephaokari@gmail.com>
2023-05-27 19:53:43 +0300

    tweak User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import hashlib
 from django.db import models
 
 class Meter(models.Model):
@@ -14,3 +15,36 @@ class Member(models.Model):
 
     def __str__(self):
         return self.name
+
+class User(models.Model):
+
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('accountant', 'Accountant'),
+        ('field_agent', 'Field Agent'),
+    ]
+    username = models.CharField(max_length=100)
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
+    password = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=15)
+    active_status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    def make_password(self, password):
+        assert password
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        return hashed_password
+
+    def save(self, *args, **kwargs):
+        # Hash the password before saving
+        if self.password:
+            self.password = self.make_password(self.password)
+        super().save(*args, **kwargs)
+
+    def verify_password(self, password):
+        # Verify the provided password against the stored hashed password
+        hashed_password = self.make_password(password)
+        return self.password == hashed_password
+
+    def __str__(self):
+        return self.username
