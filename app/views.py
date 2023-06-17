8236355d723f2dc25d35d9fd531a6c5a89d412e7
kepha-okari <kephaokari@gmail.com>
2023-06-17 14:51:13 +0300

    update the posts endpoint for invoice

--- app/views.py
+++ app/views.py
@@ -164,13 +164,6 @@ class InvoiceListAPIView(APIView):
         serializer = InvoiceSerializer(invoices, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # def post(self, request):
-    #     serializer = InvoiceSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     def post(self, request):
         serializer = InvoiceSerializer(data=request.data)
         if serializer.is_valid():
